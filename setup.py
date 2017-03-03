e886b0bc2a86dfc1fe577144a7197a79bce936a7
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-03-03 15:27:42 -0500

    Initialize package, take 2
    Also, make sure the setup.py stays in synch with changes to
    requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
+import os
+import pip
 import sys
 from setuptools import setup
 
-install_requires = ['svgwrite', 'configargparse']
+
+def parse_requirements():
+    """Return abstract requirements (without version numbers)
+    from requirements.txt.
+
+    As an exception, requirements that are URLs are used as-is.
+
+    This is tested to be compatible with pip 9.0.1.
+
+    Background: https://stackoverflow.com/a/42033122/
+    """
+
+    path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    requirements = pip.req.parse_requirements(
+        path, session=pip.download.PipSession()
+    )
+    requirements = [req.name or req.link.url for req in requirements]
+    return requirements
+
+
+install_requires = parse_requirements()
 
 if sys.version < '3.0':
     install_requires.append('mock')
